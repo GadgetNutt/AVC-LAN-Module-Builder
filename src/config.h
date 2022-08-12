882849c369309e625b1b280c4933a6239ff82534
Greg Nutt <gmnutt@hotmail.com>
2022-08-11 21:40:06 -0400

    Update config.h
    Moved AVC_BIT lengths to config.h with definitions for 8, 14.7456 and 16 MHz crystals.

--- src/config.h
+++ src/config.h
@@ -27,6 +27,13 @@
 //#define AVC_MASTER
 #define AVC_DEVICE
 //#define AVC_SNIFFER
+ 
+/* Define Crystal
+* 1	-	8 MHz
+* 2 -	14.7456 MHz
+* 3 -	16 MHz
+*/
+#define CRYSTAL		2
 
 
 #ifdef AVC_MASTER
@@ -75,5 +82,57 @@ const uint16_t  ADDR_ME = ADDR_DISPLAY_MASTER;	//	See avclan-registers.h for lis
 #define AZFM_ON  
 #define AZFM_OFF 
 
+
+/* Define Crystal
+* 1	-	8 MHz
+* 2 -	14.7456 MHz
+* 3 -	16 MHz
+* 
+* #define AVC_NORMAL_BIT_LENGTH           	Frequency * 37 * (F_CPU / 1000000L / 8) 
+* #define AVC_BIT_1_HOLD_ON_LENGTH			Frequency * 20 uS * (F_CPU / 1000000L / 8)
+* #define AVC_BIT_0_HOLD_ON_LENGTH			Frequency * 32 uS * (F_CPU / 1000000L / 8)
+* #define AVC_BIT_0_HOLD_ON_MIN_LENGTH		Frequency * 30 uS * (F_CPU / 1000000L / 8)    Compare half way between a '1' (20 us) and a '0' (32 us ): 32 - (32 - 20) /2 = 26 us
+* #define AVC_START_BIT_LENGTH				Frequency * 186 uS  * (F_CPU / 1000000L / 32) ,  prescaler 32
+* #define AVC_START_BIT_HOLD_ON_LENGTH		Frequency * 168 uS * (F_CPU / 1000000L / 32)    prescaler 32
+* #define AVC_START_BIT_HOLD_ON_MIN_LENGTH	Frequency * 44 uS * (F_CPU / 1000000L / 32)      greater that AVC_NORMAL_BIT_LENGTH,  prescaler 32
+* #define AVC_1U_LENGTH	                    Frequency * 1 uS * (F_CPU / 1000000L / 8)
+* 
+* The new values for above will need to be rounded up or down.  You may need to try both values.
+* 
+*/
+
+#if CRYSTAL == 1
+#define AVC_NORMAL_BIT_LENGTH           	0x25  // 37 * (F_CPU / 1000000L / 8) 
+#define AVC_BIT_1_HOLD_ON_LENGTH			0x14  // 20 uS * (F_CPU / 1000000L / 8) 
+#define AVC_BIT_0_HOLD_ON_LENGTH			0x20 // 32 uS * (F_CPU / 1000000L / 8)
+#define AVC_BIT_0_HOLD_ON_MIN_LENGTH		0x1E  // 30 uS * (F_CPU / 1000000L / 8)    Compare half way between a '1' (20 us) and a '0' (32 us ): 32 - (32 - 20) /2 = 26 us
+#define AVC_START_BIT_LENGTH				0x2F  // 186 uS  * (F_CPU / 1000000L / 32) ,  prescaler 32
+#define AVC_START_BIT_HOLD_ON_LENGTH		0x2A  // 168 uS * (F_CPU / 1000000L / 32)    prescaler 32
+#define AVC_START_BIT_HOLD_ON_MIN_LENGTH	0x0B  // 44 uS * (F_CPU / 1000000L / 32)      greater that AVC_NORMAL_BIT_LENGTH,  prescaler 32
+#define AVC_1U_LENGTH	                    0x01  // 1 uS * (F_CPU / 1000000L / 8)      
+#endif
+#if CRYSTAL == 2
+#define AVC_NORMAL_BIT_LENGTH           	0x44  // 37 * (F_CPU / 1000000L / 8) 
+#define AVC_BIT_1_HOLD_ON_LENGTH			0x25  // 20 uS * (F_CPU / 1000000L / 8) 
+#define AVC_BIT_0_HOLD_ON_LENGTH			0x3B // 32 uS * (F_CPU / 1000000L / 8)
+#define AVC_BIT_0_HOLD_ON_MIN_LENGTH		0x37  // 30 uS * (F_CPU / 1000000L / 8)    Compare half way between a '1' (20 us) and a '0' (32 us ): 32 - (32 - 20) /2 = 26 us
+#define AVC_START_BIT_LENGTH				0x55  // 186 uS  * (F_CPU / 1000000L / 32) ,  prescaler 32
+#define AVC_START_BIT_HOLD_ON_LENGTH		0x4D  // 168 uS * (F_CPU / 1000000L / 32)    prescaler 32
+#define AVC_START_BIT_HOLD_ON_MIN_LENGTH	0x13  // 44 uS * (F_CPU / 1000000L / 32)      greater that AVC_NORMAL_BIT_LENGTH,  prescaler 32
+#define AVC_1U_LENGTH	                    0x02  // 1 uS * (F_CPU / 1000000L / 8)      
+#endif
+#if CRYSTAL == 3
+#define AVC_NORMAL_BIT_LENGTH           	0x4A  // 37 * (F_CPU / 1000000L / 8) 
+#define AVC_BIT_1_HOLD_ON_LENGTH			0x28  // 20 uS * (F_CPU / 1000000L / 8) 
+#define AVC_BIT_0_HOLD_ON_LENGTH			0x40 // 32 uS * (F_CPU / 1000000L / 8)
+#define AVC_BIT_0_HOLD_ON_MIN_LENGTH		0x3C  // 30 uS * (F_CPU / 1000000L / 8)    Compare half way between a '1' (20 us) and a '0' (32 us ): 32 - (32 - 20) /2 = 26 us
+#define AVC_START_BIT_LENGTH				0x5D  // 186 uS  * (F_CPU / 1000000L / 32) ,  prescaler 32
+#define AVC_START_BIT_HOLD_ON_LENGTH		0x54  // 168 uS * (F_CPU / 1000000L / 32)    prescaler 32
+#define AVC_START_BIT_HOLD_ON_MIN_LENGTH	0x16  // 44 uS * (F_CPU / 1000000L / 32)      greater that AVC_NORMAL_BIT_LENGTH,  prescaler 32
+#define AVC_1U_LENGTH	                    0x02  // 1 uS * (F_CPU / 1000000L / 8)      
 #endif
 
+#define AVC_MAXMSGLEN		32
+#define AVC_CONTROL_FLAGS	0xF
+
+#endif
