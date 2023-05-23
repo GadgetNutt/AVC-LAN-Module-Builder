b2f5ccd58ef2ce198a7be6ff74fc300f08ba6879
Greg Nutt <gmnutt@hotmail.com>
2023-05-23 12:24:14 -0400

    Update config.h
    AVC_BIT_0_HOLD_ON_MIN_LENGTH update

--- src/config.h
+++ src/config.h
@@ -107,7 +107,7 @@ const uint16_t  ADDR_ME = ADDR_DISPLAY_MASTER;	//	See avclan-registers.h for lis
 #define AVC_NORMAL_BIT_LENGTH           	0x25  // 37 * (F_CPU / 1000000L / 8) 
 #define AVC_BIT_1_HOLD_ON_LENGTH			0x14  // 20 uS * (F_CPU / 1000000L / 8) 
 #define AVC_BIT_0_HOLD_ON_LENGTH			0x20 // 32 uS * (F_CPU / 1000000L / 8)
-#define AVC_BIT_0_HOLD_ON_MIN_LENGTH		0x1E  // 30 uS * (F_CPU / 1000000L / 8)    Compare half way between a '1' (20 us) and a '0' (32 us ): 32 - (32 - 20) /2 = 26 us
+#define AVC_BIT_0_HOLD_ON_MIN_LENGTH		0x16  // 30 uS * (F_CPU / 1000000L / 8)    Compare half way between a '1' (20 us) and a '0' (32 us ): 32 - (32 - 20) /2 = 26 us
 #define AVC_START_BIT_LENGTH				0x2F  // 186 uS  * (F_CPU / 1000000L / 32) ,  prescaler 32
 #define AVC_START_BIT_HOLD_ON_LENGTH		0x2A  // 168 uS * (F_CPU / 1000000L / 32)    prescaler 32
 #define AVC_START_BIT_HOLD_ON_MIN_LENGTH	0x0B  // 44 uS * (F_CPU / 1000000L / 32)      greater that AVC_NORMAL_BIT_LENGTH,  prescaler 32
@@ -117,7 +117,7 @@ const uint16_t  ADDR_ME = ADDR_DISPLAY_MASTER;	//	See avclan-registers.h for lis
 #define AVC_NORMAL_BIT_LENGTH           	0x44  // 37 * (F_CPU / 1000000L / 8) 
 #define AVC_BIT_1_HOLD_ON_LENGTH			0x25  // 20 uS * (F_CPU / 1000000L / 8) 
 #define AVC_BIT_0_HOLD_ON_LENGTH			0x3B // 32 uS * (F_CPU / 1000000L / 8)
-#define AVC_BIT_0_HOLD_ON_MIN_LENGTH		0x37  // 30 uS * (F_CPU / 1000000L / 8)    Compare half way between a '1' (20 us) and a '0' (32 us ): 32 - (32 - 20) /2 = 26 us
+#define AVC_BIT_0_HOLD_ON_MIN_LENGTH		0x2D  // 30 uS * (F_CPU / 1000000L / 8)    Compare half way between a '1' (20 us) and a '0' (32 us ): 32 - (32 - 20) /2 = 26 us
 #define AVC_START_BIT_LENGTH				0x55  // 186 uS  * (F_CPU / 1000000L / 32) ,  prescaler 32
 #define AVC_START_BIT_HOLD_ON_LENGTH		0x4D  // 168 uS * (F_CPU / 1000000L / 32)    prescaler 32
 #define AVC_START_BIT_HOLD_ON_MIN_LENGTH	0x13  // 44 uS * (F_CPU / 1000000L / 32)      greater that AVC_NORMAL_BIT_LENGTH,  prescaler 32
@@ -127,7 +127,7 @@ const uint16_t  ADDR_ME = ADDR_DISPLAY_MASTER;	//	See avclan-registers.h for lis
 #define AVC_NORMAL_BIT_LENGTH           	0x4A  // 37 * (F_CPU / 1000000L / 8) 
 #define AVC_BIT_1_HOLD_ON_LENGTH			0x28  // 20 uS * (F_CPU / 1000000L / 8) 
 #define AVC_BIT_0_HOLD_ON_LENGTH			0x40 // 32 uS * (F_CPU / 1000000L / 8)
-#define AVC_BIT_0_HOLD_ON_MIN_LENGTH		0x3C  // 30 uS * (F_CPU / 1000000L / 8)    Compare half way between a '1' (20 us) and a '0' (32 us ): 32 - (32 - 20) /2 = 26 us
+#define AVC_BIT_0_HOLD_ON_MIN_LENGTH		0x32  // 30 uS * (F_CPU / 1000000L / 8)    Compare half way between a '1' (20 us) and a '0' (32 us ): 32 - (32 - 20) /2 = 26 us
 #define AVC_START_BIT_LENGTH				0x5D  // 186 uS  * (F_CPU / 1000000L / 32) ,  prescaler 32
 #define AVC_START_BIT_HOLD_ON_LENGTH		0x54  // 168 uS * (F_CPU / 1000000L / 32)    prescaler 32
 #define AVC_START_BIT_HOLD_ON_MIN_LENGTH	0x16  // 44 uS * (F_CPU / 1000000L / 32)      greater that AVC_NORMAL_BIT_LENGTH,  prescaler 32
